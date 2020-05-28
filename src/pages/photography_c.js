f4df02510d4e05241b1c001316a27d591307b7c2
Donald Fletcher <dfletcher@whatmotivateslearning.com>
2020-05-28 09:16:27 +0100

    siteMetaData added to GraphQL query boilerplate.

--- src/pages/photography_c.js
+++ src/pages/photography_c.js
@@ -1,91 +1,143 @@
 import React from "react";
-import { graphql, StaticQuery } from "gatsby";
-
 import Layout from "../components/layout";
-import SEO from "../components/seo";
-// import Bio from "../components/bio"
-import PostCard from "../components/postCard";
+import { graphql, useStaticQuery, Link } from "gatsby";
 
-import "../style/normalize.css";
-import "../style/all.scss";
-//TODO: switch to staticQuery, get rid of comments, remove unnecessary components, export as draft template
-const BlogIndex = ({ data }, location) => {
-  const siteTitle = data.site.siteMetadata.title;
-  const posts = data.allMarkdownRemark.edges;
-  let postCounter = 0;
+import blogStyles from "../tmp/blog.module.scss";
 
-  return (
-    <Layout title={siteTitle}>
-      <SEO
-        title="Photography"
-        keywords={[`devlog`, `blog`, `gatsby`, `javascript`, `react`]}
-      />
-      {/* <Bio /> */}
-      {data.site.siteMetadata.description && (
-        <header className="page-head">
-          <h2 className="page-head-title">Photography</h2>
-        </header>
-      )}
-      <div className="post-feed">
-        {posts.map(({ node }) => {
-          postCounter++;
-          return (
-            <PostCard
-              key={node.fields.slug}
-              count={postCounter}
-              node={node}
-              postClass={`post`}
-            />
-          );
-        })}
-      </div>
-    </Layout>
-  );
-};
+import Head from "../tmp/head";
 
-const indexQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-        description
-        profession
+const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          description
+          profession
+        }
       }
-    }
-    allMarkdownRemark(
-      sort: { fields: [frontmatter___date], order: DESC }
-      filter: { fileAbsolutePath: { regex: "/blog/" } }
-    ) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM D, YYYY")
+      allContentfulBlogPost(sort: { fields: publishedDate, order: DESC }) {
+        edges {
+          node {
             title
-            description
-            tags
-            thumbnail {
-              childImageSharp {
-                fluid(maxWidth: 1360) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
+            slug
+            # publishedDate(fromNow:true)
+            publishedDate(formatString: "MMMM Do, YYYY")
           }
         }
       }
     }
-  }
-`;
+  `);
 
-export default props => (
-  <StaticQuery
-    query={indexQuery}
-    render={data => (
-      <BlogIndex location={props.location} props data={data} {...props} />
-    )}
-  />
-);
+  return (
+    <Layout title={siteTitle}>
+      {/* <Head title='Blog'/> */}
+      <h2>My Blog</h2>
+      <ol className={blogStyles.posts}>
+        {data.allContentfulBlogPost.edges.map(edge => {
+          return (
+            <li className={blogStyles.post}>
+              <Link to={`/blog/${edge.node.slug}`}>
+                <h3>{edge.node.title}</h3>
+                <p>{edge.node.publishedDate}</p>
+              </Link>
+            </li>
+          );
+        })}
+      </ol>
+    </Layout>
+  );
+};
+export default BlogPage;
+
+// import React from "react";
+// import { graphql, StaticQuery } from "gatsby";
+
+// import Layout from "../components/layout";
+// import SEO from "../components/seo";
+// // import Bio from "../components/bio"
+// import PostCard from "../components/postCard";
+
+// import "../style/normalize.css";
+// import "../style/all.scss";
+// //TODO: switch to staticQuery, get rid of comments, remove unnecessary components, export as draft template
+// const BlogIndex = ({ data }, location) => {
+//   const siteTitle = data.site.siteMetadata.title;
+//   const posts = data.allMarkdownRemark.edges;
+//   let postCounter = 0;
+
+//   return (
+//     <Layout title={siteTitle}>
+//       <SEO
+//         title="Photography"
+//         keywords={[`devlog`, `blog`, `gatsby`, `javascript`, `react`]}
+//       />
+//       {/* <Bio /> */}
+//       {data.site.siteMetadata.description && (
+//         <header className="page-head">
+//           <h2 className="page-head-title">Photography</h2>
+//         </header>
+//       )}
+//       <div className="post-feed">
+//         {posts.map(({ node }) => {
+//           postCounter++;
+//           return (
+//             <PostCard
+//               key={node.fields.slug}
+//               count={postCounter}
+//               node={node}
+//               postClass={`post`}
+//             />
+//           );
+//         })}
+//       </div>
+//     </Layout>
+//   );
+// };
+
+// const indexQuery = graphql`
+//   query {
+//     site {
+//       siteMetadata {
+//         title
+//         description
+//         profession
+//       }
+//     }
+//     allMarkdownRemark(
+//       sort: { fields: [frontmatter___date], order: DESC }
+//       filter: { fileAbsolutePath: { regex: "/blog/" } }
+//     ) {
+//       edges {
+//         node {
+//           excerpt
+//           fields {
+//             slug
+//           }
+//           frontmatter {
+//             date(formatString: "MMMM D, YYYY")
+//             title
+//             description
+//             tags
+//             thumbnail {
+//               childImageSharp {
+//                 fluid(maxWidth: 1360) {
+//                   ...GatsbyImageSharpFluid
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
+
+// export default props => (
+//   <StaticQuery
+//     query={indexQuery}
+//     render={data => (
+//       <BlogIndex location={props.location} props data={data} {...props} />
+//     )}
+//   />
+// );
